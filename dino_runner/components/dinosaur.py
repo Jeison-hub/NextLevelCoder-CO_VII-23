2ea9a58c30304d9de216bdad1e4a8e274a2ccffa
JeisonVillada <jeisonvillada13@gmail.com>
2023-07-13 18:58:27 -0500

    Class 1: introduccion

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,3 @@
+# crear clase dinosaur
+# añadir un __init__
+# añadir metodos de UPDATE,DRAW
--- main.py
+++ main.py
@@ -3,4 +3,9 @@ from dino_runner.components.game import Game
 if __name__ == "__main__":
     game = Game()
     game.run()
-    print("hello there...")
+
+
+    # ===== Investigar Herencia
+    # == que es un sprite. pygame rect
+    # == 
+    
